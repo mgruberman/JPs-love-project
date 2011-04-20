20d299bbd0b412f8db2d44e6d16be5ad409154f1
dan baumfeld <thethirddan@gmail.com>
2011-04-20 01:47:43 -0700

    I hate heroku

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find_by_slug(params[:id])
-    @reviews = Review.find(:all, :conditions => ["user_id = :user_id", :user_id => @user.id], :include => :shop)
+    @reviews = Review.find(:all, :conditions => {:user_id => @user.id}, :include => :shop)
 
     if @user.nil?
       redirect_to '/'
